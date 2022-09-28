dd90ce4fe745a72d48de88c07b7fc70067f37c80
test12301232 <test12301232@gmail.com>
2022-09-28 12:25:47 +0300

    new

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,9 @@ app.use('/', express.static(resolve(__dirname, '../public')));
 app.get('*', (req, res) => res.sendFile(resolve(__dirname, '../public/index.html')));
 app.use(ErrorMiddleware);
 
+console.log(resolve(__dirname, '../public'));
+console.log(__dirname);
+
 
 const start = async (): Promise<void> => {
    try {
