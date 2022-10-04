55021e5962319bcad63e53a07be08c18c6e63585
test12301232 <test12301232@gmail.com>
2022-10-04 12:16:01 +0300

    new

--- src/exeptions/ApiError.ts
+++ src/exeptions/ApiError.ts
@@ -1,22 +1,22 @@
 export default class ApiError extends Error {
-   readonly status;
-   readonly errors;
+    readonly status;
+    readonly errors;
 
-   constructor(status: number, message: string, errors: Array<any> = []) {
-       super(message);
-       this.status = status;
-       this.errors = errors;
-   }
+    constructor(status: number, message: string, errors: Array<any> = []) {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+    }
 
-   static UnauthorizedError() {
-       return new ApiError(401, 'Пользователь не авторизован!');
-   }
+    static UnauthorizedError() {
+        return new ApiError(401, 'Пользователь не авторизован!');
+    }
 
-   static BadRequest(message: string, errors: Array<any> = []) {
-       return new ApiError(400, message, errors);
-   }
+    static BadRequest(message: string, errors: Array<any> = []) {
+        return new ApiError(400, message, errors);
+    }
 
-   static AccessDenied(){
-       return new ApiError(403, 'Forbidden');
-   }
+    static AccessDenied() {
+        return new ApiError(403, 'Forbidden');
+    }
 }
